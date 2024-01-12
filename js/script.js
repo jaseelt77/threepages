8f5e3cc828b2ee848b98af9f07b1314b303d167b
Muhammed <muhammedramees1998@gmail.com>
2024-01-12 20:26:54 +0530

    homepage

---
+++ js/script.js
@@ -0,0 +1,27 @@
+let navbar = document.querySelector('.header .navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () =>{
+  navbar.classList.remove('active');
+};
+
+
+// const tl = gsap.timeline();
+
+// tl.from(".content h1", 1.8, {
+//   y: 100,
+//   ease: "power4.out",
+//   delay: 1,
+//   skewY: 7,
+//   stagger: {
+//     amount: 0.3
+//   }
+// })
+
+$( document ).ready(function() {
+  AOS.init();
+});
+
